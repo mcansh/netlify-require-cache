c9fa0e19e84b94ac0fff7f9849fdc85f9211425a
Logan McAnsh <logan@mcan.sh>
2021-09-22 16:29:19 -0400

    chore: return html
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,39 @@
 import { Handler, HandlerResponse } from "@netlify/functions";
 
+function createHTML(text: string): string {
+  return `
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+      </head>
+      <body>
+        ${text}
+      </body>
+    </html>
+  `;
+}
+
 function hello(): HandlerResponse {
   return {
-    statusCode: 200,
-    body: "Hello from Netlify Functions!",
+    statusCode: 201,
+    body: createHTML("Hello from Netlify Functions!"),
+    multiValueHeaders: {
+      "Content-Type": ["text/html"],
+    },
   };
 }
 
 function goodbye(): HandlerResponse {
   return {
     statusCode: 200,
-    body: "Goodbye from Netlify Functions!",
+    body: createHTML("Goodbye from Netlify Functions!"),
+    multiValueHeaders: {
+      "Content-Type": ["text/html"],
+    },
   };
 }
 
@@ -18,13 +41,20 @@ function index(): HandlerResponse {
   return {
     statusCode: 200,
     multiValueHeaders: {
-      "Set-Cookie": ["cookie1=value=1", "cookie2=value=2", "cookie3=value=3"],
+      "Content-Type": ["text/html"],
+      "Set-Cookie": [
+        "cookie1=value1",
+        "cookie2=value2",
+        "cookie3=value3",
+        "cookie4=value4",
+        "cookie5=value5",
+      ],
     },
-    body: "Check those cookies",
+    body: createHTML("Check those cookies"),
   };
 }
 
-const routes: { [path: string]: () => HandlerResponse } = {
+const routes: { [path: string]: () => HandlerResponse | undefined } = {
   "/hello": hello,
   "/goodbye": goodbye,
   "/": index,
@@ -36,7 +66,7 @@ const server: Handler = (event) => {
   if (!route) {
     return {
       statusCode: 404,
-      body: "Not found",
+      body: createHTML("Not found"),
     };
   }
 
